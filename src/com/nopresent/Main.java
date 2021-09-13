07e7507985dde063a78b1f691761f8f6351eff63
nopresent <nopresent@gmail.com>
2021-09-13 22:34:01 +0300

    просто коммит

--- src/com/nopresent/Main.java
+++ src/com/nopresent/Main.java
@@ -8,5 +8,9 @@ public class Main {
         System.out.println("3");
         System.out.println("4");
         System.out.println("5");
+
+        System.out.println("7");
+        System.out.println("8");
+
     }
 }
