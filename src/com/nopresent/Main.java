e67fcca28f3174e1677746937f66fbb6ad587b55
nopresent <nopresent@gmail.com>
2021-09-13 22:39:13 +0300

    просто коммит

--- src/com/nopresent/Main.java
+++ src/com/nopresent/Main.java
@@ -2,15 +2,8 @@ package com.nopresent;
 
 public class Main {
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Test");
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
-        System.out.println("5");
 
-        System.out.println("7");
-        System.out.println("8");
+        System.out.println("Test");
 
     }
 }
